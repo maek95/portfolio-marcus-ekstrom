4c9a624307028199b494c378dee4d5b96c43ffaf
maek95 <mekstrom01@gmail.com>
2024-03-19 16:09:45 +0100

    preview image of project, responsive project sect

--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 import ThemeButton from "@/components/ThemeButton";
 import Header from "../components/Header";
 import FontButton from "../components/FontButton";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
 import Section from "../components/Section";
-import Link from "next/link";
 import ProjectAndContactButton from "@/components/ProjectAndContactButton";
-import { useRef } from 'react';
+import { useRef } from "react";
+import Project from "@/components/Project";
 
 /* 
 const inter = Inter({ subsets: ["latin"] }); */
 
 export default function Home() {
-
   const projectsSectionRef = useRef(null);
   const aboutMeSectionRef = useRef(null);
   const heroSectionRef = useRef(null);
@@ -21,79 +20,104 @@ export default function Home() {
   function scrollToSection(sectionRef) {
     const yOffset = -80; // Adjust this value to change the offset
     const element = sectionRef.current;
-    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-    window.scrollTo({ top: y, behavior: 'smooth' });
-  };
+    const y =
+      element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: "smooth" });
+  }
 
   return (
-    
     <div className="text-black dark:text-white bg-[#EEEFF2] dark:bg-[#1A1C21] min-h-screen">
-      <Header onClickHome={() => {
-        scrollToSection(heroSectionRef)
-      }} onClickAbout={() => {
-        scrollToSection(aboutMeSectionRef)
-      }} onClickProjects={() => scrollToSection(projectsSectionRef)}>
+      <Header
+        onClickHome={() => {
+          scrollToSection(heroSectionRef);
+        }}
+        onClickAbout={() => {
+          scrollToSection(aboutMeSectionRef);
+        }}
+        onClickProjects={() => scrollToSection(projectsSectionRef)}
+      >
         <ThemeButton />
-     </Header>
-    
-    <div className="" ref={heroSectionRef}></div>
-    <Hero onClick={() => scrollToSection(projectsSectionRef)}></Hero>
-    
-    
-   {/*  <main
+      </Header>
+
+      <div className="" ref={heroSectionRef}></div>
+      <Hero onClick={() => scrollToSection(projectsSectionRef)}></Hero>
+
+      {/*  <main
       className={`pt-[5rem] pb-[0.5rem] bg-[#EEEFF2] dark:bg-[#1A1C21] ${inter.className}`}
     > */}
-    <main
-      className={`bg-white dark:bg-[#23272F]`}
-    >
-      <div className="" ref={aboutMeSectionRef}></div>
-      <Section title="ABOUT ME" bgcolor={"[#f7f8fc]"} darkbgcolor={"[#23272F]"}>
-        <div className="flex flex-col items-start text-start gap-6">
-          <h4>Get to know me!</h4>
-          <div className="sm:w-[70%] md:w-[60%]">
-            <p className="">I am in the final stages of studying Frontend Developing in mobile & web-applications at Chas Academy (university) in Stockholm. We have worked with everything from CSS and HTML to JavaScript, React, Nextjs, ...fillinblank... Prior to my coding journey I have taken a <u>Masters in Economics</u> at Stockholm University, and I have taken university-courses in different coding languages (e.g. C++, Java, Kotlin, etc.)  </p>
-          </div>
-         {/*  <a href="#projects"> */}
-            <ProjectAndContactButton title={"CONTACT"}></ProjectAndContactButton>
-         {/*  </a> */}
-          
-        </div>
-
-      </Section>
-
-    {/*   <a id="projects"></a> */}
-    <div className="" ref={projectsSectionRef}></div>
-    <Section title={"PROJECTS"} bgcolor={"[#EEEFF2]"} darkbgcolor={"[#1A1C21]"}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-          <Card>
-            <div className="min-w-full flex items-center gap-8">
-              <h3>Project 1</h3>
-              <div className="flex h-full flex-col items-start text-start">
-                <Link href={""}>
-                  <ProjectAndContactButton title={"Project Link"}></ProjectAndContactButton>
-                </Link>
-                <p >This project was in the final stages of my studies at Chas Academy</p>
-                <img src="" alt="<preview img>" />
-              </div>
+      <main className={`bg-white dark:bg-[#23272F]`}>
+        <div className="" ref={aboutMeSectionRef}></div>
+        <Section
+          title="ABOUT ME"
+          bgcolor={"[#f7f8fc]"}
+          darkbgcolor={"[#23272F]"}
+        >
+          <div className="flex flex-col items-start text-start gap-6">
+            <h4>Get to know me!</h4>
+            <div className="sm:w-[70%] md:w-[60%]">
+              <p className="">
+                I am in the final stages of studying Frontend Developing in
+                mobile & web-applications at Chas Academy (university) in
+                Stockholm. We have worked with everything from CSS and HTML to
+                JavaScript, React, Nextjs, ...fillinblank... Prior to my coding
+                journey I have taken a <u>Masters in Economics</u> at Stockholm
+                University, and I have taken university-courses in different
+                coding languages (e.g. C++, Java, Kotlin, etc.){" "}
+              </p>
             </div>
-          </Card>
-          <Card><h3>Project 2</h3></Card>
-          <Card><h3>Project 3</h3></Card>
-          <Card><h3>Project 4</h3></Card>
-        </div>
-      </Section>
-      <Card>
-        <FontButton /> {/* // remove? */}
-      </Card>
+            {/*  <a href="#projects"> */}
+            <ProjectAndContactButton
+              title={"CONTACT"}
+            ></ProjectAndContactButton>
+            {/*  </a> */}
+          </div>
+        </Section>
 
-      {/* <Card>
+        {/*   <a id="projects"></a> */}
+        <div className="" ref={projectsSectionRef}></div>
+        <Section
+          title={"PROJECTS"}
+          bgcolor={"[#EEEFF2]"}
+          darkbgcolor={"[#1A1C21]"}
+        >
+          <div className="grid grid-cols-1 gap-5">
+            <Project
+              title={"Quiz App"}
+              description={
+                "This project was in the final stages of my studies at Chas Academy"
+              }
+              projectLink={"https://23-chas-quiz.vercel.app"}
+            ></Project>
+
+            <Project
+              title={"Figma App"}
+              description={
+                "At Chas Academy we had a 1-week segment about Figma and I made a simple App!"
+              }
+              projectLink={
+                "https://www.figma.com/proto/7EXISjrUDnc5rQEhkxgSAX/my-Health-app?node-id=19-297&starting-point-node-id=19%3A297&mode=design&t=JFTobLpRmywpSry4-1"
+              }
+            ></Project>
+            <Project
+              title={"Replicated Nordea's website"}
+              description={
+                
+                  "Simple HTML & CSS project I made during one of the first workshops at Chas Academy. Tried replicating Nordea's feedback page https://www.nordea.se/privat/kundservice/synpunkter-och-forslag.html"               
+               
+              }
+              projectLink={"https://workshop-4-form.vercel.app"}
+              imgSrc={"/NordeaPagePreview.png"}
+            ></Project>
+          </div>
+        </Section>
+        <Card>
+          <FontButton /> {/* // remove? */}
+        </Card>
+
+        {/* <Card>
         <h3>Project 1</h3>
       </Card> */}
-    </main>
-
+      </main>
     </div>
-    
   );
 }
-
-
---
+++ components/Project.jsx
@@ -0,0 +1,25 @@
+import Card from "./Card"
+import ProjectAndContactButton from "./ProjectAndContactButton"
+
+export default function Project({title, description, projectLink, imgSrc}){
+
+  return (
+    <Card>
+      <div className="min-w-full flex flex-col lg:flex-row items-center gap-8">
+        <div className="flex w-full justify-center">
+
+          <img className="w-auto h-[250px] md:h-[300px] lg:h-[450px] rounded-lg" src={imgSrc} alt="<preview img>" />
+        </div>
+
+        <div className="flex h-full flex-col items-center lg:items-start text-center lg:text-start gap-4">
+          <h3>{title}</h3>
+
+          <p className="text-[#7c7c7c]">{description}</p>
+          <a href={projectLink} target="_blank">
+            <ProjectAndContactButton title={"Project Link"}></ProjectAndContactButton>
+          </a>
+        </div>
+      </div>
+    </Card>
+  )
+}
