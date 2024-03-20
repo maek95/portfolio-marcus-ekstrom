b468330e36f73371c9bfc6e63356609d54b4adbb
maek95 <mekstrom01@gmail.com>
2024-03-20 12:27:18 +0100

    again..

--- app/preventZoom.jsx
+++ app/preventZoom.jsx
@@ -1,42 +1,20 @@
 "use client"
 // preventZoom.jsx
+// preventZoom.jsx
 import { useEffect } from "react";
 
 export default function PreventZoom() {
   useEffect(() => {
-    const preventZoom = (e) => {
+    const handleGestureStart = (e) => {
       if (e.scale !== 1) {
-        // Check if the user is trying to zoom
         e.preventDefault();
-        const newZoom = document.body.style.zoom || 1; // Get the current zoom level or default to 1
-        const maxZoom = 1; // Set the maximum allowed zoom level (in this case, preventing zoom out)
-        const minZoom = 1; // Set the minimum allowed zoom level (optional, can be adjusted as needed)
-        const delta = e.scale > 1 ? 0.01 : -0.01; // Adjust the delta value for zoom in/out
-        
-        // Prevent zooming out beyond the minimum allowed zoom level
-        if (e.scale < 1 && parseFloat(newZoom) <= minZoom) {
-          return;
-        }
-
-        // Prevent zooming in beyond the maximum allowed zoom level
-        if (e.scale > 1 && parseFloat(newZoom) >= maxZoom) {
-          return;
-        }
-
-        // Apply the zoom change
-        const updatedZoom = parseFloat(newZoom) + delta;
-        document.body.style.zoom = updatedZoom;
       }
     };
 
-    document.documentElement.addEventListener("gesturestart", preventZoom);
-    document.documentElement.addEventListener("gesturechange", preventZoom);
-    document.documentElement.addEventListener("gestureend", preventZoom);
+    document.documentElement.addEventListener("gesturestart", handleGestureStart);
 
     return () => {
-      document.documentElement.removeEventListener("gesturestart", preventZoom);
-      document.documentElement.removeEventListener("gesturechange", preventZoom);
-      document.documentElement.removeEventListener("gestureend", preventZoom);
+      document.documentElement.removeEventListener("gesturestart", handleGestureStart);
     };
   }, []);
 
