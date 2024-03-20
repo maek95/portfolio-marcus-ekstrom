f22fcdce4eac0f9d6d012c506953ccc890c63390
maek95 <mekstrom01@gmail.com>
2024-03-20 12:08:10 +0100

    aaa

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,7 @@ export const metadata = {
  
 export const viewport = {
   width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
+  initialScale: 0.5,
   userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
