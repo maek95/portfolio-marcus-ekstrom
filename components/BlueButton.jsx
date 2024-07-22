f2a4b60e759ae43cc06b9f1756b34081cfcdee3d
maek95 <mekstrom01@gmail.com>
2024-07-22 13:08:39 +0200

    more smallerSize terniary styling BlueButton

--- components/BlueButton.jsx
+++ components/BlueButton.jsx
@@ -6,7 +6,7 @@ export default function BlueButton({ isGitHubLink = false, title, onClick, small
 
     <button
       onClick={onClick}
-      className={`flex flex-col gap-2 border-none items-center justify-center rounded-lg bg-[#347FC1] hover:bg-[#3f97e4] ${smallerSize ? "py-2 px-4 text-sm" : "py-4 px-12 text-xl"} text-white font-sourceSans cursor-pointer`}
+      className={`flex flex-col gap-2 border-none items-center justify-center bg-[#347FC1] hover:bg-[#3f97e4] ${smallerSize ? "py-2 px-4 text-sm rounded-lg" : "py-3 px-12 text-xl rounded-xl"} text-white font-sourceSans cursor-pointer`}
     >
       <div className="flex items-center gap-4">
      {isGitHubLink && <svg
